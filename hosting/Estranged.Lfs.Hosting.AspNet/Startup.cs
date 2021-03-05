3f6b56577173622cd905997883b6796472745561
Stephan van Stekelenburg <stephan@hayman.io>
2021-03-06 00:31:16 +0100

    feat: enable asw/az blob/gh/bb feaures from aspnet

--- hosting/Estranged.Lfs.Hosting.AspNet/Startup.cs
+++ hosting/Estranged.Lfs.Hosting.AspNet/Startup.cs
@@ -7,31 +7,102 @@ using Microsoft.Extensions.Configuration;
 using Estranged.Lfs.Adapter.S3;
 using Estranged.Lfs.Data;
 using System.Collections.Generic;
+using System;
+using System.Linq;
+using Estranged.Lfs.Authenticator.GitHub;
+using Estranged.Lfs.Authenticator.BitBucket;
+using Estranged.Lfs.Adapter.Azure.Blob;
 
 namespace Estranged.Lfs.Hosting.AspNet
 {
-    public class Startup
+  public class Startup
+  {
+    public void ConfigureServices(IServiceCollection services)
     {
-        public void ConfigureServices(IServiceCollection services)
+      services.AddLogging(x =>
+      {
+        x.AddConsole();
+        x.AddDebug();
+      });
+
+      const string LfsBucketVariable = "LFS_BUCKET";
+      const string LfsUsernameVariable = "LFS_USERNAME";
+      const string LfsPasswordVariable = "LFS_PASSWORD";
+      const string GitHubOrganisationVariable = "GITHUB_ORGANISATION";
+      const string GitHubRepositoryVariable = "GITHUB_REPOSITORY";
+      const string BitBucketWorkspaceVariable = "BITBUCKET_WORKSPACE";
+      const string BitBucketRepositoryVariable = "BITBUCKET_REPOSITORY";
+      const string S3AccelerationVariable = "S3_ACCELERATION";
+      const string LfsAzureStorageConnectionStringVariable = "LFS_AZUREBLOB_CONNECTIONSTRING";
+      const string LfsAzureStorageContainerNameVariable = "LFS_AZUREBLOB_CONTAINERNAME";
+
+      var config = new ConfigurationBuilder()
+          .AddEnvironmentVariables()
+          .Build();
+
+      string lfsBucket = config[LfsBucketVariable];
+      string lfsAzureStorageConnectionString = config[LfsAzureStorageConnectionStringVariable];
+      string lfsUsername = config[LfsUsernameVariable];
+      string lfsPassword = config[LfsPasswordVariable];
+      string gitHubOrganisation = config[GitHubOrganisationVariable];
+      string gitHubRepository = config[GitHubRepositoryVariable];
+      string bitBucketWorkspace = config[BitBucketWorkspaceVariable];
+      string bitBucketRepository = config[BitBucketRepositoryVariable];
+      bool s3Acceleration = bool.Parse(config[S3AccelerationVariable] ?? "false");
+
+      bool isS3Storage = !string.IsNullOrWhiteSpace(lfsBucket);
+      bool isAzureStorage = !string.IsNullOrWhiteSpace(lfsAzureStorageConnectionString);
+      bool isDictionaryAuthentication = !string.IsNullOrWhiteSpace(lfsUsername) && !string.IsNullOrWhiteSpace(lfsPassword);
+      bool isGitHubAuthentication = !string.IsNullOrWhiteSpace(gitHubOrganisation) && !string.IsNullOrWhiteSpace(gitHubRepository);
+      bool isBitBucketAuthentication = !string.IsNullOrWhiteSpace(bitBucketWorkspace) && !string.IsNullOrWhiteSpace(bitBucketRepository);
+
+      // If all authentication mechanims are set, or none are set throw an error
+      if (new[] { isDictionaryAuthentication, isGitHubAuthentication, isBitBucketAuthentication }.Count(x => x) != 1)
+      {
+        throw new InvalidOperationException($"Unable to detect authentication mechanism. Please set {LfsUsernameVariable} and {LfsPasswordVariable} for simple user/password auth" +
+                                            $" or {GitHubOrganisationVariable} and {GitHubRepositoryVariable} for authentication against that repository on GitHub");
+      }
+
+      if (isDictionaryAuthentication)
+      {
+        services.AddLfsDictionaryAuthenticator(new Dictionary<string, string> { { lfsUsername, lfsPassword } });
+      }
+
+      if (isGitHubAuthentication)
+      {
+        services.AddLfsGitHubAuthenticator(new GitHubAuthenticatorConfig { Organisation = gitHubOrganisation, Repository = gitHubRepository });
+      }
+
+      if (isBitBucketAuthentication)
+      {
+        services.AddLfsBitBucketAuthenticator(new BitBucketAuthenticatorConfig { Workspace = bitBucketWorkspace, Repository = bitBucketRepository });
+      }
+
+      if (isS3Storage)
+      {
+        services.AddLfsS3Adapter(new S3BlobAdapterConfig { Bucket = lfsBucket }, new AmazonS3Client(new AmazonS3Config { UseAccelerateEndpoint = s3Acceleration }));
+      }
+      else if (isAzureStorage)
+      {
+        services.AddLfsAzureBlobAdapter(new AzureBlobAdapterConfig
         {
-            IConfiguration credentials = new ConfigurationBuilder().AddJsonFile("credentials.json").Build();
+          ConnectionString = lfsAzureStorageConnectionString,
+          ContainerName = config[LfsAzureStorageContainerNameVariable]
+        });
+      }
+      else
+      {
+        throw new InvalidOperationException($"Missing environment variable {LfsBucketVariable} or {LfsAzureStorageConnectionStringVariable}.");
+      }
 
-            services.AddLogging(x =>
-            {
-                x.AddConsole();
-                x.AddDebug();
-            });
+      services.AddLfsApi();
 
-            services.AddSingleton<IAmazonS3, AmazonS3Client>();
-            services.AddLfsS3Adapter(new S3BlobAdapterConfig{Bucket = "estranged-lfs-test"}, new AmazonS3Client());
-            services.AddSingleton<IAuthenticator>(x => new DictionaryAuthenticator(new Dictionary<string, string> { { "usernametest", "passwordtest" } }));
-            services.AddLfsApi();
-        }
-
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseRouting();
-            app.UseEndpoints(endpoints => endpoints.MapControllers());
-        }
     }
+
+    public void Configure(IApplicationBuilder app)
+    {
+      app.UseRouting();
+      app.UseEndpoints(endpoints => endpoints.MapControllers());
+    }
+  }
 }
