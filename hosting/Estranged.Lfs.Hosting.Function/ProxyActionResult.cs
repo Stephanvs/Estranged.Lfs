a6307ed29d06a7c418877a22289edae4795fda58
Stephan van Stekelenburg <stephan@hayman.io>
2021-03-10 16:34:43 +0100

    chore: pass along HttpContext from function app

--- hosting/Estranged.Lfs.Hosting.Function/ProxyActionResult.cs
+++ hosting/Estranged.Lfs.Hosting.Function/ProxyActionResult.cs
@@ -11,11 +11,7 @@ namespace Estranged.Lfs.Hosting.Function
         {
             context.HttpContext.Features.Set<IServiceProvidersFeature>(null); // remove webjob host service provider
 
-            await InternalServer.Instance.Application.ProcessRequestAsync(
-                new Microsoft.AspNetCore.Hosting.Internal.HostinApplication.Context()
-                {
-                    HttpContext = context.HttpContext
-                });
+            await InternalServer.Instance.Application.ProcessRequestAsync(context.HttpContext);
         }
     }
 }
